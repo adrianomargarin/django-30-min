5f199c8383a380b69c691a53934f76e8e1bc78d4
Adriano Margarin <adriano.margarin@gmail.com>
2018-03-16 12:53:30 -0300

    Teste PDF

--- django_30_min/urls.py
+++ django_30_min/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_30_min.core.topdf import Pdf
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pdf/', Pdf.as_view()),
 ]
